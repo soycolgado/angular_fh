79a1a3591b6fa80d8954caa4b44f15cfd5eb3826
Nestor Bardel <nestorbardel@gmail.com>
2025-10-31 19:00:19 -0300

    Refactor HeroPage component: update template structure, fix method name, and enhance functionality

--- 02-bases/src/app/pages/hero/hero-page.component.ts
+++ 02-bases/src/app/pages/hero/hero-page.component.ts
@@ -1,11 +1,39 @@
-import { Component } from "@angular/core";
+import { UpperCasePipe } from "@angular/common";
+import { Component, computed, Signal, signal, WritableSignal } from "@angular/core";
 
 
 @Component({
   selector: 'app-hero-page',
   templateUrl: './hero-page.component.html',
+  imports:[UpperCasePipe]
   // styleUrls: ['./hero-page.component.css']
 })
 export class HeroPageComponent {
-  // Component logic goes here
+  name: WritableSignal<string> = signal('Ironman');
+  age: WritableSignal<number> = signal(45);
+
+  heroDescription: Signal<string> = computed(() => {
+    const description = `${this.name()} - ${this.age()}`;
+    return description;
+  });
+
+  capitalizedName: Signal<string> = computed(() => this.name().toUpperCase());
+
+  // getHeroDescription(): string{
+  //   return `${this.name()} - ${this.age()}`;
+  // }
+
+  changeHero():void{
+    this.name.set("Spiderman");
+    this.age.set(22);
+  }
+
+  resetForm():void{
+    this.name.set("Ironman");
+    this.age.set(45);
+  }
+
+  changeAge():void{
+    this.age.set(60);
+  }
 }
