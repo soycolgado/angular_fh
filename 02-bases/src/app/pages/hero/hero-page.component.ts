ca32b7b1a45bf2df65c035213539f35f3c26ef66
Nestor Bardel <nestorbardel@gmail.com>
2025-10-31 00:15:08 -0300

    Add HeroPage component and update routing
    - Created HeroPageComponent with associated HTML template.
    - Updated app routing to include a path for the HeroPage component.

---
+++ 02-bases/src/app/pages/hero/hero-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+  selector: 'app-hero-page',
+  templateUrl: './hero-page.component.html',
+  // styleUrls: ['./hero-page.component.css']
+})
+export class HeroPageComponent {
+  // Component logic goes here
+}
